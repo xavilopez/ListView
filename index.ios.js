81ff2133e55e143847d6e248382440b2b7125f8f
xavilopez <xavi_lopez011@hotmail.com>
2016-09-10 12:47:43 -0400

    Added ListView
    Added ListView

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {
+  AppRegistry
+} from 'react-native';
+
+import BookListV2 from './BookListV2';
+
+AppRegistry.registerComponent('ListView', () => BookListV2);
